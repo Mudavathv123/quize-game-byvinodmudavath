c7e676ca84f1df95007315b90d0596e2e48ae6f4
Mudavathv123 <mudavathvinod49@gmail.com>
2024-10-09 14:45:23 +0530

    update style

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -5,7 +5,7 @@ import "./../App.css"
 const MainScreen = props => {
 
   const { players, currentQuestion } = props;
-  const qrUrl = window.location.origin + '/mobile';
+  const qrUrl = `${window.location.origin}/mobile`;
 
   console.log(qrUrl)
 
@@ -13,23 +13,25 @@ const MainScreen = props => {
   console.log(currentQuestion)
 
   return (
-    <div className="">
+    <div>
       <h1 className='main-screen-heading'>Quiz Game - Main Screen</h1>
-      <div className='qr-code-container'>
-        <QRCodeCanvas className='qr-code' value={qrUrl} size={256} />
-        <h2 className='player-joined'>Players Joined:</h2>
-        <ol>
-          {players.map((player, index) => (
-            <li key={index} className='list-item'>{player}</li>
-          ))}
-        </ol>
-      </div>
-      <div className='question-container'>
-        <h2 className= "question">{currentQuestion.question}</h2>
-        <div className='options-container'>
-          {currentQuestion.options.map((option, index) => (
-            <button key={index} className='option'>{option}</button>
-          ))}
+      <div className="container">
+        <div className='qr-code-container'>
+          <QRCodeCanvas className='qr-code' value={qrUrl} size={256} />
+          <h2 className='player-joined'>Players Joined:</h2>
+          <ol>
+            {players.map((player, index) => (
+              <li key={index} className='list-item'>{player}</li>
+            ))}
+          </ol>
+        </div>
+        <div className='question-container'>
+          <h2 className="question">{currentQuestion.question}</h2>
+          <div className='options-container'>
+            {currentQuestion.options.map((option, index) => (
+              <button key={index} className='option'>{option}</button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
